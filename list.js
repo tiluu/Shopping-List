71735808a644c5a0f3c2e2419eb61a37c6378b50
tina.p.luu@gmail.com <tina.p.luu@gmail.com>
2013-02-12 23:03:42 -0500

    modified css

--- list.js
+++ list.js
@@ -7,28 +7,29 @@ $(document).ready(function(){
 		$("table#shopping_list tbody").append("<tr>"+newNumber +newItem+newCheck+"</tr>");
 	};
 
-	$("#add_item").click(function(){
-		var user_number=$("#number").val();
-		var user_item=$("#item").val();
-		if(user_item==""&&user_number==""){
-			alert("What do you need?");
-		} 
-		else if (user_number ==""){
-			alert("How much are you buying?");
+	
+	$('input').keypress(function(e){
+		if(e.which== 13){
+			event.preventDefault();
+			var user_number=$("#number").val();
+			var user_item=$("#item").val();
+			if(user_item==""&&user_number==""){
+				alert("What do you need?");
+			} 
+			else if (user_number ==""){
+				alert("How much are you buying?");
+			}
+			else if(user_item==""){
+				alert("What are you buying?");
+			}
+			else{
+				addRow(user_number,user_item);	
+				$('tr input:checkbox').on('click', function(){
+					$(this).closest("tr").remove();
+				});
+				document.forms[0].reset();
+			}
 		}
-		else if(user_item==""){
-			alert("What are you buying?");
-		}
-		else{
-			addRow(user_number,user_item);	
-			$('tr input:checkbox').on('click', function(){
-				$(this).closest("tr").remove();
-			});
-			document.forms[0].reset();
-		}
-
 	});
 
 });
-
-
